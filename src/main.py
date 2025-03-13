2320a91a9db40a5511447f030c547340adc14d75
tayadj <tayadj@users.noreply.github.com>
2025-03-13 18:37:03 +0300

    Implemented Bot instance

--- src/main.py
+++ src/main.py
@@ -1,8 +1,39 @@
 import config
 import core
 
+import aiogram
+import asyncio
+
 
 
 class Bot():
 
-	pass
+	def __init__(self, settings):
+
+		self.bot = aiogram.Bot(token = settings.TELEGRAM_TOKEN.get_secret_value())
+		self.dispatcher = aiogram.Dispatcher()
+
+		self.handlers_setup()
+
+	def handlers_setup(self):
+
+		@self.dispatcher.message(aiogram.filters.Command('start'))
+		async def handle_command_start(message: aiogram.types.Message):
+
+			reflexion = await core.handlers.handle_command_start(message)
+
+	async def run(self):
+
+		await self.dispatcher.start_polling(self.bot)
+
+
+
+if __name__ == '__main__':
+
+	async def main():
+
+		settings = config.Settings()
+		bot = Bot(settings)
+		await bot.run()
+
+	asyncio.run(main())
