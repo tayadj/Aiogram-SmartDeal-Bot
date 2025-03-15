2e8e299bef51737172d7261ff4cf0e90c6d2204d
tayadj <tayadj@users.noreply.github.com>
2025-03-15 16:00:14 +0300

    Miscellaneous

--- src/config/settings.py
+++ src/config/settings.py
@@ -6,11 +6,11 @@ import pydantic_settings
 
 class Settings(pydantic_settings.BaseSettings):
 	
-	model_config = pydantic_settings.SettingsConfigDict(
+    model_config = pydantic_settings.SettingsConfigDict(
 		env_file = os.path.dirname(__file__) + '/.env',
 		env_file_encoding = 'utf-8',
 		extra = 'ignore'
-	)
+    )
 
-	OPENAI_API_TOKEN: pydantic.SecretStr
-	TELEGRAM_TOKEN: pydantic.SecretStr
+    OPENAI_API_TOKEN: pydantic.SecretStr
+    TELEGRAM_TOKEN: pydantic.SecretStr
